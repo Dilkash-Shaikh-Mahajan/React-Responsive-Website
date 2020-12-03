9c14e0a48419980bb87818681cc1068b6fa6a5fe
Dilkash-web-design <61229215+Dilkash-web-design@users.noreply.github.com>
2020-12-03 23:38:31 +0530

    first commit

---
+++ src/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Footer = () => {
+	return (
+		<>
+			<footer className='w-100 py-3 bg-light text-center'>
+				<p>
+					© 2020{' '}
+					<a href='https://dilkashshaikhmahajan.co.in/'>
+						Dilkash Shaikh Mahajan
+					</a>
+					. All Rights Reserved | Terms and Conditions
+				</p>
+			</footer>
+		</>
+	);
+};
+
+export default Footer;
